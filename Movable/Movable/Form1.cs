01d4ee7b61f2257507c9453d99e4daace8fa003b
Koson Trachu <koson.trachu@gmail.com>
2016-09-13 23:55:46 +0700

    movable

---
+++ Movable/Movable/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Movable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public interface IMovable
+    {
+        void Walk();
+        void Run();
+        void Swim();
+    }
+}
